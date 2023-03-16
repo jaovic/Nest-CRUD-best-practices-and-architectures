d00eacb432205980d0a4b055caa5b2edb65650db
João Victor Martins <j_victorfernandes@hotmail.com>
2023-03-16 11:26:20 -0300

    🏷️ add find user service type

--- src/modules/user/structure/IService.structure.ts
+++ src/modules/user/structure/IService.structure.ts
@@ -5,6 +5,14 @@ export interface ICreateUser {
   document: string;
   email: string;
 }
+
+export interface IFindUser {
+  user_id: string;
+}
+
 export interface ICreateUserService {
   execute(data: ICreateUser): Promise<users>;
+}
+export interface IFindUserService {
+  execute(data: IFindUser): Promise<users>;
 }
