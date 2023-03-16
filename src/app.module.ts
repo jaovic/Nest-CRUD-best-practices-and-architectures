b4faf9b9d9d5ed1b7ac5d3af5f89ca391e9de3ab
João Victor Martins <j_victorfernandes@hotmail.com>
2023-03-16 16:07:16 -0300

    🚧 add providers in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AddressModule } from './modules/address/address.module';
 import { UserModule } from './modules/user/user.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AddressModule],
   controllers: [],
   providers: [PrismaService],
 })
